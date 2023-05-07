609408cb3bc1431809950b9a2b5d1fdaaf2032b3
Alişan <alisanakbaba2003@hotmail.com>
2023-05-07 23:14:46 +0300

    vucutKitkeIndeksiHesaplama
    kullanıcı boyunu ve kilosunu girdiğinde vücut kitle indeksini hesaplayan program

---
+++ vucutKitleIndeksiHesaplama/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // girdi almak icin input nesnesini kullanacagiz
+        Scanner input = new Scanner(System.in);
+        // degiskenleri tanimladik
+        int kilo;
+        double boy,result;
+        // degiskenleri tanimladik
+        System.out.print("Lutfen kilonuzu giriniz: ");
+        kilo = input.nextInt();
+        System.out.print("Lutfen boyunuzu (metre cinsinden) giriniz: ");
+        boy = input.nextDouble();
+        // vucut kitle indeksini hesaplayan formul
+        result = kilo/(boy*boy);
+        //sonuc bolumu
+        System.out.println("Vucut kitle indeksiniz: "+result);
+
+    }
+}
